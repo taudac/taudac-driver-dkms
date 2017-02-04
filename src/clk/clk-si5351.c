ec698060113f6fed4718adcfbe5de52b20450a7a
Sergej Sawazki <taudac@gmx.de>
2017-02-04 17:20:12 +0100

    clk: si5351: Do not enable parent clocks in probe()

--- src/clk/clk-si5351.c
+++ src/clk/clk-si5351.c
@@ -1416,9 +1416,9 @@ static int si5351_i2c_probe(struct i2c_client *client,
 	}
 
 	if (!IS_ERR(drvdata->pxtal))
-		clk_prepare_enable(drvdata->pxtal);
+		clk_prepare(drvdata->pxtal);
 	if (!IS_ERR(drvdata->pclkin))
-		clk_prepare_enable(drvdata->pclkin);
+		clk_prepare(drvdata->pclkin);
 
 	/* register xtal input clock gate */
 	memset(&init, 0, sizeof(init));
@@ -1599,9 +1599,9 @@ static int si5351_i2c_probe(struct i2c_client *client,
 
 err_clk:
 	if (!IS_ERR(drvdata->pxtal))
-		clk_disable_unprepare(drvdata->pxtal);
+		clk_unprepare(drvdata->pxtal);
 	if (!IS_ERR(drvdata->pclkin))
-		clk_disable_unprepare(drvdata->pclkin);
+		clk_unprepare(drvdata->pclkin);
 	return ret;
 }
 
