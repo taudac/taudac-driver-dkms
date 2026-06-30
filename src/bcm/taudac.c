96406a1114c1ae130b258251198755cd15308c8b
Sergej Sawazki <ce3a@gmx.de>
2026-06-30 21:43:59 +0200

    taudac: use inclusive language for SND_SOC_DAIFMT_CBx_CFx
    Fixup commit: bb64be2ce210 ("taudac: use inclusive language for
    SND_SOC_DAIFMT_CBx_CFx")

--- src/bcm/taudac.c
+++ src/bcm/taudac.c
@@ -534,7 +534,7 @@ static struct snd_soc_dai_link taudac_dai[] = {
 		.stream_name   = "TauDAC HiFi",
 		.dai_fmt       = SND_SOC_DAIFMT_I2S |
 				 SND_SOC_DAIFMT_NB_NF |
-				 SND_SOC_DAIFMT_CBS_CFS,
+				 SND_SOC_DAIFMT_CBC_CFC,
 		.playback_only = true,
 		.ops  = &taudac_ops,
 		.init = taudac_init,
